adbbc9e3ea361ed10c1e8bb491c7406ba04a1ea7
Yann Leflour <yann@ublo.immo>
2023-11-29 12:33:30 +0100

    fix executable's index.ts path

--- exec.cjs
+++ exec.cjs
@@ -3,8 +3,9 @@ const { execSync } = require("child_process");
 const path = require("path");
 
 const tsxPath = path.join(__dirname, "./node_modules/.bin/tsx");
+const indexTsPath = path.join(__dirname, "./index.ts");
 try {
-  execSync(`${tsxPath} ./index.ts`, { stdio: "inherit" });
+  execSync(`${tsxPath} ${indexTsPath}`, { stdio: "inherit" });
 } catch (e) {
   // Gracefully exit if needed
   process.exit(1);
