eff51b6d12c299dd425125a4378a23c150fd117a
Yann Leflour <yleflour@gmail.com>
2024-05-14 15:09:17 +0200

    move base to gpt4o

--- src/assistants/base-assistant.ts
+++ src/assistants/base-assistant.ts
@@ -1,6 +1,7 @@
 import { AssistantConfig } from "../assistant.utils.js";
 import { appendToFile } from "../tools/appendToFile.js";
 import { executeCommand } from "../tools/executeCommand.js";
+import { getUrlContent } from "../tools/getUrlContent.js";
 import { ls } from "../tools/ls.js";
 import { readFile } from "../tools/readFile.js";
 import { writeFile } from "../tools/writeFile.js";
@@ -8,7 +9,7 @@ import { writeFile } from "../tools/writeFile.js";
 export default {
   name: "baseAssistant" as const,
   description: "",
-  model: "gpt-4-turbo-preview",
+  model: "gpt-4o",
   instructions: `You are a helpful assistant. Use any tools available to help the user`,
-  tools: [readFile, ls, writeFile, executeCommand, appendToFile],
+  tools: [readFile, ls, writeFile, executeCommand, appendToFile, getUrlContent],
 } satisfies AssistantConfig;
