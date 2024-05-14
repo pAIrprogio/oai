2fed8045f853aa9a4c6bc385fee6392af30dc2c8
Yann Leflour <yleflour@gmail.com>
2024-05-14 15:09:54 +0200

    add url content fetching

---
+++ src/tools/getUrlContent.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+import { Tool } from "../tool.utils.js";
+import axios from "axios";
+
+const argsSchema = z.object({
+  url: z.string(),
+});
+
+type Args = z.input<typeof argsSchema>;
+
+interface ToMdResponse {
+  metadata: {
+    url?: string;
+    title?: string;
+    authors: string[];
+    publication_date?: string;
+    top_image?: string;
+  };
+  content: string;
+}
+
+export const getUrlContent = {
+  name: "getUrlContent",
+  description: "Fetches the content of a URL",
+  argsSchema,
+  async call(args: Args) {
+    const res = await axios.post<ToMdResponse>(
+      "https://ai-toolkit.pairprog.io/2md/article-url",
+      {
+        url: args.url,
+      },
+      {
+        headers: {
+          Authorization: process.env.AI_TOOLKIT_API_KEY,
+        },
+      },
+    );
+
+    return {
+      success: true,
+      output: res.data,
+    };
+  },
+} satisfies Tool<Args>;
