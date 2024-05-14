0da6fd2402c3adca5b2f0782e4830fcfa78c8a21
Yann Leflour <yleflour@gmail.com>
2024-05-14 15:09:07 +0200

    add gpt-4o

--- src/assistant.utils.ts
+++ src/assistant.utils.ts
@@ -2,7 +2,7 @@ import { Tool, toOpenAiTools } from "./tool.utils.js";
 
 export interface AssistantConfig<T extends Array<Tool> = Array<Tool<any>>> {
   name: string;
-  model: "gpt-4-turbo-preview" | "gpt-3.5-turbo";
+  model: "gpt-4-turbo-preview" | "gpt-3.5-turbo" | "gpt-4o";
   description: string;
   instructions: string;
   tools: T;
